287fc0598cfbde15d3bb0a8e62c5ce94578cddcb
Michael Vezzani <mjvezzani@gmail.com>
2014-03-12 12:35:54 -0400

    Initial commit. Install bootstrap. Create main_pages controller and index view. Add initial text.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapPoc::Application.config.secret_key_base = '95a5e1094c28787e6aa701f11e8f1ca109cc12861642f4a0f34790e77a7d31446a0a2802e9728500af4096a21a468d12337bbc84a59030023915d6131235be11'
